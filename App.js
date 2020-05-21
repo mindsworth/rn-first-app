a8879919b2c1e68bcec5ddc52fca77a380ada643
Chigoziem Nwaiwu <princegoziem@gmail.com>
2020-05-21 05:36:59 +0100

    Update:: change text on button press

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [changeText, setChangeText] = useState(
+    "Open up App.js to start working on your app!"
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{changeText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setChangeText("The text changed!")}
+      />
     </View>
   );
 }
@@ -12,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
